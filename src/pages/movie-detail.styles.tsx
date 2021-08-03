665f0ce921d14d56e768eadf5fc1886f37cdabf9
jorge-cob <jorge.cobreros@protonmail.com>
2021-08-03 18:00:26 +0200

    restyled detail view

---
+++ src/pages/movie-detail.styles.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+
+export const DetailContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+
+  .header {
+    text-align: center;
+  }
+  .content-container {
+    justify-content:center;
+    align-items:center;
+    display: inline-block;
+    text-align: center;
+
+  }
+  .background-image {
+    height: 600px;
+
+    background-position: center;
+    background-repeat: no-repeat;
+  } 
+  .image-container {
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+
--- src/pages/movie-detail.tsx
+++ src/pages/movie-detail.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RouteComponentProps } from "react-router-dom";
 import { getMovieById } from '../redux/movie/movie.selectors';
+import { DetailContainer } from './movie-detail.styles';
 
 
 
@@ -14,25 +15,32 @@ const MovieDetails: React.FC<Movie> = (props) => {
   const movieDetails = useSelector((state) => getMovieById(state, itemId));
 
   return (
-    <div>
-      <h1>{movieDetails.title}</h1>
-      <p>
-        <strong>Release date:</strong> {movieDetails.release_date}
-      </p>
-      <p>
-        <strong>Overview:</strong> {movieDetails.overview}
-      </p>
-      <p>
-        <strong>Poster:</strong> 
-        <img 
-          alt={movieDetails.title}
-          src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${movieDetails.poster_path}`} 
-        />
-      </p>
-      <p>
-        <strong>Vote average:</strong> {movieDetails.vote_average}
-      </p>
-    </div>
+    <DetailContainer>
+      <div className='header'>
+        <h1>{movieDetails.title}</h1>
+      </div>
+      <div className='content-container'>
+        <div className='image-container'>
+          <div 
+            className='background-image'
+            style={{
+              backgroundImage: `url(https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movieDetails.poster_path})`
+            }}
+          />
+        </div>
+        <div className='content'>
+          <p>
+            <strong>Release date:</strong> {movieDetails.release_date}
+          </p>
+          <p>
+            <strong>Overview:</strong> {movieDetails.overview}
+          </p>
+          <p>
+            <strong>Vote average:</strong> {movieDetails.vote_average}
+          </p>
+        </div>
+      </div>
+    </DetailContainer>
   );
 }
 
